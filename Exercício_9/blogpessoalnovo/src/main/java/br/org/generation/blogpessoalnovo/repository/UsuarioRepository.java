7ca26967acffcfa3acd6b95bba82b28701a4cce4
Gabriel Marques <gabriel-vanda@hotmail.com>
2022-02-03 13:38:57 -0300

    Deploy

---
+++ Exercício_9/blogpessoalnovo/src/main/java/br/org/generation/blogpessoalnovo/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.org.generation.blogpessoalnovo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.org.generation.blogpessoalnovo.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase(String nome);
+}
